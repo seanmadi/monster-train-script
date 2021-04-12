f171e8a2fd5564811dfcc7dee0684dcd289892b2
Sean Madigan <seanmadi@gmail.com>
2021-04-12 09:37:32 -0400

    I don't know what I did, just throwing this up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* global shell, process */
+
+import React from "react"
+import {
+  Container,
+  FormControl,
+  Button,
+  InputLabel,
+  Input,
+  FormHelperText,
+} from "@material-ui/core"
 
 function App() {
+  const onClickOauth = (event) => {
+    event.preventDefault()
+    shell.openExternal(event.target.href)
+  }
+
+  const defaultFilePath =
+    "C:\\Users\\" +
+    (process.env.username || process.env.user) +
+    "\\AppData\\LocalLow\\Shiny Shoe\\MonsterTrain\\saves\\save-singlePlayer.json"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <h1>Monster Train Twitch Bot</h1>
+
+      <form>
+        <FormControl>
+          <InputLabel htmlFor="username">Twitch username</InputLabel>
+          <Input id="username" aria-describedby="username" />
+        </FormControl>
+
+        <FormControl>
+          <InputLabel htmlFor="channel">Channel</InputLabel>
+          <Input id="channel" aria-describedby="channel" />
+          <FormHelperText>
+            The Twitch Channel the bot should be listening on (this is likely
+            your Twitch channel name).
+          </FormHelperText>
+        </FormControl>
+
+        <FormControl>
+          <InputLabel htmlFor="oauth">Oauth token</InputLabel>
+          <Input id="oauth" aria-describedby="oauth" />
+          <FormHelperText>
+            Retrieve your token by visiting and logging into:
+            <a
+              className="external-link"
+              href="https://twitchapps.com/tmi"
+              onClick={onClickOauth}
+            >
+              https://twitchapps.com/tmi
+            </a>
+          </FormHelperText>
+        </FormControl>
+
+        <FormControl>
+          <InputLabel htmlFor="saveFilePath">Save File Path</InputLabel>
+          <Input
+            id="saveFilePath"
+            aria-describedby="saveFilePath"
+            defaultValue={defaultFilePath}
+          />
+        </FormControl>
+
+        <Button color="primary">Run</Button>
+      </form>
+    </Container>
+  )
 }
 
-export default App;
+export default App
